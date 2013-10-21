1f8a84b6fb43df25eb51a071bf3071e65b421ae1
Florian Demmer <fdemmer@gmail.com>
2013-10-22 00:03:16 +0200

    + some more setup.py improvements

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+import io
 from distutils.core import setup
 
-def requirements(filename="requirements.txt"):
-    with file(filename) as f:
-        return [line.strip() for line in f]
+import togglcli
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+requirements = read('requirements.txt')
+
 
 setup(name='togglcli',
-    version='0.1',
-    description='',
-    author='',
-    author_email='',
-    url='',
-    install_requires=requirements(),
+    version=togglcli.__version__,
+    description='Rough around the edges toggl.com commandline client.',
+    long_description=long_description,
+    license='GNU General Public License, version 2.0 (GPLv2)',
+    author='@mos3abof et al.',
+    url='https://github.com/fdemmer/toggl_target/',
+    install_requires=requirements,
     packages = [
         'targetlib',
         'targetlib.togglapi',
